f5d9cbe6931ccfb2b4e398b8d37449a48516b4ba
Rolf Kleef <r.kleef@nivocer.com>
2011-09-08 23:41:27 +0200

    rough first sketch of home page (incomplete)

---
+++ sketches/land-on-home-page_files/story.js
@@ -0,0 +1,11 @@
+var story = {
+ "title": "land-on-home-page",
+ "pages": [{
+  "title": "home",
+  "height": 1249,
+  "width": 960,
+  "image": "home.png",
+  "links": []
+ }],
+ "highlightLinks": false
+}
